0f77b3aff01c31a1f020151d3a401d634cebe399
hassanelijah02 <hassanelijah02@gmail.com>
2023-12-11 04:15:13 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// const navToggle = document.querySelector('.nav-toggle');
+// const nav = document.querySelector('.nav');
+// const  hamburger = document.querySelector('.hamburger');
+
+
+// navToggle.addEventListener('click', () => {
+//     nav.classList.toggle('nav--visible');
+//     navToggle.classList.toggle('active');
+// })
+
+// const myString = 'developer';
+// myString.charAt(0).toUpperCase;
